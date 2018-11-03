f30ed1a63d45e9838fb7a36e9e7b85c02d659b65
bandolero32 <44655343+bandolero32@users.noreply.github.com>
2018-11-04 01:10:49 +0300

    Update mac.py

--- mac.py
+++ mac.py
@@ -8,29 +8,10 @@ a=1
 while a < 2:
     print("Mac adresi değiştiriliyor...")
     a=a+1
-    os.system("ifconfig wlan1 down")
-    os.system("macchanger -r wlan1")
+    os.system("ifconfig wlan0 down")
+    os.system("macchanger -r wlan0")
     os.system("service networking restart")
     os.system("service network-manager restart")
-
-b=1
-while b < 2:
-    print("Mac adresi değiştiriliyor...")
-    b=b+1
-    os.system("ifconfig wlan1 down")
-    os.system("macchanger -r wlan1")
-    os.system("service networking restart")
-    os.system("service network-manager restart")
-
-c=1
-while c < 2:
-    print("Mac adresi değiştiriliyor...")
-    c=c+1
-    os.system("ifconfig wlan1 down")
-    os.system("macchanger -r wlan1")
-    os.system("service networking restart")
-    os.system("service network-manager restart")
-
 
 print("Mac adresi değiştirildi !")
 
